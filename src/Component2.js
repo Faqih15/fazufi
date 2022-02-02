5d454029eee2b35f8dfdec738a494405b274cd17
faqih15 <jahidiyam1592@gmail.com>
2022-02-02 15:35:40 +0700

    destructure props

--- src/Component2.js
+++ src/Component2.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-export default function Component2(props) {
-    const ganti = ()=>{
-        props.setAngka("angka diganti lewat props")
-    }
+export default function Component2({ contohVariable, angk, setAngka, fungsi }) {
+  const ganti = () => {
+    props.setAngka("angka diganti lewat props");
+  };
   return <div onClick={ganti}>component2</div>;
 }
